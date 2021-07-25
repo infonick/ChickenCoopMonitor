ca52065f369eb037fbf3de7bbb00ec991c148423
Infonick <49381882+infonick@users.noreply.github.com>
2021-07-25 15:09:34 -0700

    Added Pico Setup Script

---
+++ PicoSetupScript.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+RED='\033[0;31m' # Red Text Color
+NC='\033[0m' # No Text Color
+
+printf "Updating the system software...\n"
+sudo apt update -y
+sudo apt upgrade -y
+
+printf "Installing any missing dependencies...\n"
+sudo apt install git -y
+sudo apt install python3 -y
+sudo apt install python3-pip -y
+sudo pip3 install rshell
+
+# Check that user belongs to dialout group.
+if ! id -nGz "$USER" | grep -qzxF dialout
+then
+    printf "Adding user '$USER' to the 'dialout' group... ${RED}"
+    sudo usermod -aG dialout $USER
+	printf "${NC}done.\n"
+fi
+
+
+printf "Copying files to Pi Pico... \n"
+sudo find $HOME/ChickenCoopMonitor/Pico\ Code/ -type d -exec rshell cp {} +
+sudo find $HOME/ChickenCoopMonitor/Pico\ Code/ -type f -exec rshell cp {} +
